f520c33feca8424e5d54a892aa71fb5eaa88e428
Kaushik Biswas <kaushik.biswas@outlook.com>
2021-02-28 15:24:31 +0530

    Updated GRPC files

--- protobuf/generate.bat
+++ protobuf/generate.bat
@@ -1,1 +1,4 @@
-protoc.exe -I src\ --go_out=src\ src\simple\simple.proto
+protoc.exe -I .\ --go_out=.\ .\simple\simple.proto
+protoc.exe -I .\ --go_out=.\ .\enum_example\enum_example.proto
+protoc.exe -I .\ --go_out=.\ .\complex\complex.proto
+protoc.exe .\greet\greet.proto --go_out=.\ --go-grpc_out=.
